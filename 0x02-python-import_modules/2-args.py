6d51af161309b8fe3aee85e6a07b5e225b3128c6
m33x0 <michael.mbea@gmail.com>
2020-12-10 17:44:36 +0100

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     arg_count = len(sys.argv) - 1
     if arg_count == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif arg_count == 1:
         print("1 argument:")
     else:
