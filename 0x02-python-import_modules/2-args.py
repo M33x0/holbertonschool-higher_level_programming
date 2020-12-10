5c2dfe1bb51783990878e140afb40aa9820c568c
m33x0 <michael.mbea@gmail.com>
2020-12-10 17:42:20 +0100

    fix variable name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len = len(sys.argv) - 1
-    if len == 0:
+    arg_count = len(sys.argv) - 1
+    if arg_count == 0:
         print("0 argument.")
-    elif len == 1:
+    elif arg_count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len))
-    for i in range(1, len+1):
+        print("{} arguments:".format(arg_count))
+    for i in range(1, arg_count+1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
