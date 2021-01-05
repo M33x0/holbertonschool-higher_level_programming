57053e2703cbcc629a7f601e555d25c0a7809085
m33x0 <michael.mbea@gmail.com>
2021-01-05 16:16:31 +0100

    square definition with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Empty module"""
+
+
+class Square:
+    """Define a square shape"""
+    def __init__(self, size):
+        self.__size = size
+
+
+# help(Square) square class documentation
