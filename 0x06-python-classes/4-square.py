3966ec57942dd16aa2da74ee2541842fcc4c3969
m33x0 <michael.mbea@gmail.com>
2021-01-05 18:11:32 +0100

    access and update attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Square class - setter and initialize the class
+"""
+
+
+class Square:
+    """
+        Square class
+    """
+    def __init__(self, value=0):
+        """ Initialize attributs
+        Args: value (int): size of the square
+        """
+        self._size = value
+
+    @property
+    def size(self):
+        """ getter of size
+        """
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """ Setter of size
+        Args: value (int): value to check
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self._size * self._size
