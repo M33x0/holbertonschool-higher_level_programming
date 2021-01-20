3c0644e338ff34df4df77183cc0f3583ce58c877
m33x0 <michael.mbea@gmail.com>
2021-01-20 22:39:57 +0100

    fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """append a text file and return numbers character written """
+    """append a string to a text file """
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
 
