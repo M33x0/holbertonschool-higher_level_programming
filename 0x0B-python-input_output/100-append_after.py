52b5df9f40fea689194a8b5747d4a74ba8773554
m33x0 <michael.mbea@gmail.com>
2021-01-20 23:54:42 +0100

    refact

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,9 @@
 def append_after(filename="", search_string="", new_string=""):
     """ append_after """
     line = ""
-    with open(filename, 'r', encoding='utf-8') as p:
+    with open(filename, mode='r', encoding='utf-8') as p:
         line = p.readlines()
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         for i in line:
             if search_string in i:
                 i += new_string
