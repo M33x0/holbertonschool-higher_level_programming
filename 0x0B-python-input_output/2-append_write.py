a3141b427723917632e83e33e2de61dc1b64483f
m33x0 <michael.mbea@gmail.com>
2021-01-20 21:04:04 +0100

    ffs removed semi colon

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@
 
 def append_write(filename="", text=""):
    """append a text file and return numbers character written """
-   number_of_characters = 0;
+   number_of_characters = 0
    with open(filename, 'a', encoding='utf-8') as f:
            number_of_characters = f.write(text)
    return number_of_characters
