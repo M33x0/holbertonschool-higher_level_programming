ed967abdb1c31357c2fbdcb4b8712770ef634786
m33x0 <michael.mbea@gmail.com>
2021-01-20 22:29:04 +0100

    fix

--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Input/Output"""
-
-
-def write_file(filename="", text=""):
-   """write text in file"""
-   with open(filename, mode='w', encoding='utf-8') as f:
-      return f.write(text)
