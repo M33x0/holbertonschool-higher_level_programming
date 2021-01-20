57deccca17a29a41aff6decf91accb0207c5ae6d
m33x0 <michael.mbea@gmail.com>
2021-01-20 23:48:29 +0100

    search n destroy

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""insert line of text to a file after """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ append_after """
+    line = ""
+    with open(filename, 'r', encoding='utf-8') as p:
+        line = p.readlines()
+    with open(filename, 'w', encoding='utf-8') as f:
+        for i in line:
+            if search_string in i:
+                i += new_string
+            f.write(i)
