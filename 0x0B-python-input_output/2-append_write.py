456e9d83fbddcac5aa7876981142bb3c9e34e6ff
m33x0 <michael.mbea@gmail.com>
2021-01-20 17:24:02 +0100

    append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Input/Output """
+
+def append_write(filename="", text=""):
+   """append a text file and return numbers character written """
+   number_of_characters = 0;
+   with open(filename, 'a', encoding='utf-8') as f:
+           number_of_characters = f.write(text)
+   return number_of_characters
