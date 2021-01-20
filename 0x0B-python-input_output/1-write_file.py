a90f1f6ff700231561b1d52e4f0e847b89b3b640
m33x0 <michael.mbea@gmail.com>
2021-01-20 21:54:48 +0100

    fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,7 @@
 
 
 def write_file(filename="", text=""):
-   """write a string to a text file and return 
-   numbers character written """
+   """ write text in file """
    number_of_characters = 0
    with open(filename, mode='w', encoding='utf-8') as f:
         number_of_characters = f.write(text)
