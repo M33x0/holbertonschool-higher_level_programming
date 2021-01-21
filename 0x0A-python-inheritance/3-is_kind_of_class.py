b880ed5ea71b16f26bc0c155bccb64545f0800d5
m33x0 <michael.mbea@gmail.com>
2021-01-21 18:49:09 +0100

    ffs

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python - Inheritance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return True if object is instance
+    of/or if the object is an instance
+    of a class that inherit """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        False
