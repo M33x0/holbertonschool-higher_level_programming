0a21e94891496d419302c1c42aa0198292293061
M33x0 <michael.mbea@gmail.com>
2021-03-02 22:29:25 +0100

    fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,5 +4,5 @@ SELECT		tv_shows.title, tv_show_genres.genre_id
 FROM		tv_shows
 LEFT JOIN	tv_show_genres
 ON			tv_show_genres.show_id = tv_shows.id
-ORDER BY	tv_showsg.gtitleg ASC,
+ORDER BY	tv_shows.title ASC,
 			tv_show_genresg.genre_id ASC;
