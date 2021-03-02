49187db16ca27ea8ecddb320f64ca86c74b76da0
M33x0 <michael.mbea@gmail.com>
2021-03-02 21:57:40 +0100

    fix states FK

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- create database and table with foreign keys
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities'(
 	id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
