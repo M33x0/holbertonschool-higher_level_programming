18567a5cd3bc6ab90f618c2bdaab73dc8b34e469
M33x0 <michael.mbea@gmail.com>
2021-03-02 21:30:44 +0100

    AUTO_INCREMENT

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- 6. States table
+-- create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL
+);
