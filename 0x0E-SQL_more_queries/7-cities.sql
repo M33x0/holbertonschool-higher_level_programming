828668359c881a6e6312b9b6e8818e6298f48abf
M33x0 <michael.mbea@gmail.com>
2021-03-02 21:46:08 +0100

    fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT PRIMARY KEY AUTOINCREMENT NOT NULL,
-	state_id NOT NULL FOREIGN KEY (state_id) REFERENCES city(id),
-	name VARCHAR(256) NOT NULL
+	state_id NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.cities(id)
 );
