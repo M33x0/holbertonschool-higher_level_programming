68eaca382bc4373a7aa858f2e6feab965fee1cca
M33x0 <michael.mbea@gmail.com>
2021-03-02 23:00:24 +0100

    fix

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,9 +2,9 @@
 -- List all comedy shows in db hbtn_od_tvshows
 SELECT	s.title
 FROM		tv_shows AS s
-			INNER JOIN tv_show_genres AS sg
-			ON s.id = sg.show_id
-			INNER JOIN tv_genres AS g
-			ON sg.genre_id = g.id
+INNER JOIN 	tv_show_genres AS sg
+ON 			s.id = sg.show_id
+INNER JOIN 	tv_genres AS g
+ON 			sg.genre_id = g.id
 WHERE		g.name = "Comedy"
 ORDER BY	s.title ASC;
