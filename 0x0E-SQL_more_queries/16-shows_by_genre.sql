85dd60acdf5bfc9ff7c0cecb61bce97ecafcc50c
M33x0 <michael.mbea@gmail.com>
2021-03-02 23:00:05 +0100

    fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,9 +2,9 @@
 -- list all shows and all genres linked to that show from db hbtn_od_tvshows
 SELECT		s.title, g.name
 FROM		tv_shows AS s
-			LEFT JOIN tv_show_genres AS sg
-			ON s.id = sg.show_id
-			LEFT JOIN tv_genres AS g
-			ON sg.genre_id =  g.id
+LEFT JOIN 	tv_show_genres AS sg
+ON 			s.id = sg.show_id
+LEFT JOIN 	tv_genres AS g
+ON 			sg.genre_id = g.id
 ORDER BY	s.title ASC,
 			g.name ASC;
