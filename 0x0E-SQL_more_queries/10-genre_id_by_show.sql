dcaad458a0c6a957b1be72704afa93350aa8bfa5
M33x0 <michael.mbea@gmail.com>
2021-03-02 22:21:34 +0100

    LEFT JOIN

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- 10. Genre ID by show
+-- list all shows contained in hbtn_od_tvshows
+SELECT   tv_shows.title, tv_show_genres.genre_id
+FROM     tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE    tv_show_genres.genre_id IS NOT NULL
+ORDER BY  tv_shows.title ASC,
+		tv_show_genres.genre_id ASC;
