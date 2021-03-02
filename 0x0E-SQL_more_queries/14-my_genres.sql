341e23a794e734bf361518a4617cc119446f171d
M33x0 <michael.mbea@gmail.com>
2021-03-02 22:58:41 +0100

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,9 +2,9 @@
 -- use the db hbtn_od_tvshows to list genres DEXTER
 SELECT		g.name
 FROM		tv_genres AS g
-			INNER JOIN tv_show_genres AS sg
-			ON g.id = sg.genre_id
-			INNER JOIN tv_shows AS s
-			ON sg.show_id = s.id
-WHERE		s.title = Dexter
+INNER JOIN 	tv_show_genres AS sg
+ON 			g.id = sg.genre_id
+INNER JOIN 	tv_shows AS s
+ON 			sg.show_id = s.id
+WHERE		s.title = "Dexter"
 ORDER BY	g.name ASC;
