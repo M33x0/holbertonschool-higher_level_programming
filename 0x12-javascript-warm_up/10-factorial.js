cc4ddf40d63199304ef8c786b67d329491458887
M33x0 <michael.mbea@gmail.com>
2021-03-22 19:30:01 +0100

    factorialize

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+function factorial(number) {
+	
+	if (number < 0)
+	{
+		return (-1);
+	}
+	else if (number === 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (number * factorial(number -1))
+	}
+}
+
+let args1 = parseInt(process.argv[2]);
+if (isNaN(args1))
+{
+	console.log(1);
+}
+else
+{
+	console.log(factorial(args1));
+}
