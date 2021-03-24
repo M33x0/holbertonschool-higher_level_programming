2d3283f1623cc072af6ffe119098550b1084786b
M33x0 <michael.mbea@gmail.com>
2021-03-24 20:57:07 +0100

    changed let to const

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let fs = require('fs');
-let args = process.argv;
-let file1 = fs.readFileSync(args[2], 'utf8');
-let file2 = fs.readFileSync(args[3], 'utf8');
-let data = file1 + file2;
+const fs = require('fs');
+const args = process.argv;
+const file1 = fs.readFileSync(args[2], 'utf8');
+const file2 = fs.readFileSync(args[3], 'utf8');
+const data = file1 + file2;
 fs.writeFile(args[4], data, err => {
   if (err) {
     console.error(err);
