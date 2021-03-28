f7625b472c5e4f28e330e8f19cbf12f64d6d14cb
M33x0 <michael.mbea@gmail.com>
2021-03-28 23:37:46 +0200

    fix indent

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,21 @@ if __name__ == '__main__':
     u_name = sys.argv[1]
     passwd = sys.argv[2]
     db_name = sys.argv[3]
-    conn = MySQLdb.connect(host='localhost', port=3306, user=u_name,
-                                passwd=passwd, db=db_name, charset='utf8')
+
+    conn = MySQLdb.connect(host='localhost',
+                                port=3306,
+                                user=u_name,
+                                passwd=passwd,
+                                db=db_name,
+                                charset='utf8'
+                                )
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT *\
+                FROM states\
+                WHERE name LIKE BINARY 'N%'\
+                ORDER BY id ASC"
+                )
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
