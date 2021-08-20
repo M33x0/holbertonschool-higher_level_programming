4dfe41cc078e94765352f973669cab315a2c382e
M33x0 <michael.mbea@gmail.com>
2021-08-20 23:43:06 +0200

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+"""
+   send a post request
+"""
+from urllib import request, parse
+import sys
+if __name__ == "__main__":
+    data = parse.urlencode({"email": sys.argv[2]})
+    data = data.encode('ascii')
+    with request.urlopen(sys.argv[1], data) as response:
+        html = response.read()
+    print(html.decode("utf8"))
