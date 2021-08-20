bd0edf9f53ab37047a2f1fb258a20f210e2cd2e5
M33x0 <michael.mbea@gmail.com>
2021-08-20 23:44:18 +0200

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    request, print body or catch error
+"""
+from urllib import request, parse, error
+
+import sys
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+        print(html.decode('utf8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
